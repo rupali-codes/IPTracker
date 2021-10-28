2a5c8031053241bd22e8c44e78d9a473295db40b
Rupali <78981177+rupali18082003@users.noreply.github.com>
2021-10-28 23:12:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const ipAdd = document.querySelector('#ip');
+const go = document.querySelector('#go');
+const showIP = document.querySelector('.ip'); 
+const address = document.querySelector('.add'); 
+const timezone = document.querySelector('.timezone'); 
+const isp = document.querySelector('.isp'); 
+
+
+go.addEventListener('click', async function(){
+	const ip = ipAdd.value;
+	
+	fetch(`http://ip-api.com/json/${ip}`)
+	.then(res => res.json())
+	.then(data => {
+		if(data.status == "fail"){
+			throw new Error("Invalid IP Address");
+		}
+
+		showIP.textContent = data.query;
+		address.textContent = data.city + ", " + data.country;
+		timezone.textContent = data.timezone;
+		isp.textContent = data.isp;
+	})
+	.catch(err => alert(err.message))
+
+	// const res = await fetch(`http://ip-api.com/json/${ip}`);
+	// const data = await res.json();
+	// if(!data){
+	// 	throw new Error("Invalid IP Address");
+	// }
+	// showIP.textContent = data.query;
+	// address.textContent = data.city + ", " + data.country;
+	// timezone.textContent = data.timezone;
+	// isp.textContent = data.isp;
+
+
+})
+
+/*
+//mine
+49.15.226.69
+//pinku
+102.64.48.0
+*/
