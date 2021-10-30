61b5b7acbd75822e8707ad3e22963f3cfc647e3d
Rupali <78981177+rupali18082003@users.noreply.github.com>
2021-10-30 11:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,32 @@
-
-// const ipAdd = document.querySelector('#ip');
-// const go = document.querySelector('#go');
-// const showIP = document.querySelector('.ip');
-// const address = document.querySelector('.add');
-// const timezone = document.querySelector('.timezone');
-// const isp = document.querySelector('.isp');
-
-//This is yours rups you can Edit your script file if you wish CONGO samosa Lover 
-
-// go.addEventListener('click', async function() {
-//     const ip = ipAdd.value;
-
-//     fetch(`http://ip-api.com/json/${ip}`)
-//         .then(res => res.json())
-//         .then(data => {
-//             if (data.status == "fail") {
-//                 throw new Error("Invalid IP Address");
-//             }
-
-//             showIP.textContent = data.query;
-//             address.textContent = data.city + ", " + data.country;
-//             timezone.textContent = data.timezone;
-//             isp.textContent = data.isp;
-//         })
-//         .catch(err => alert(err.message))
-// })
-
-// /*
-// //mine
-// 49.15.226.69
-// //pinku
-// 102.64.48.0
-// */
-
 const ipAdd = document.querySelector('#ip');
 const go = document.querySelector('#go');
 const showIP = document.querySelector('.ip'); 
 const address = document.querySelector('.add'); 
 const timezone = document.querySelector('.timezone'); 
-const isp = document.querySelector('.isp'); 
+const cc = document.querySelector('.cc'); 
 
 
-go.addEventListener('click', function(){
+go.addEventListener('click', async function(){
 	const ip = ipAdd.value;
 	
-	fetch(`http://ip-api.com/json/${ip}`)
+	fetch(`https://ipapi.co/${ip}/json/`)
 	.then(res => res.json())
 	.then(data => {
 		if(data.status == "fail"){
 			throw new Error("Invalid IP Address");
 		}
 
-		showIP.textContent = data.query;
-		address.textContent = data.city + ", " + data.country;
+		showIP.textContent = data.ip;
+		address.textContent = data.city + ", " + data.region + ", " + data.country_name;
 		timezone.textContent = data.timezone;
-		isp.textContent = data.isp;
+		cc.textContent = data.org;
 	})
 	.catch(err => alert(err.message))
-
 })
 
-
+/*
+//mine
+49.15.226.69
+//pinku
+102.64.48.0
+*/
