a377f5df05690d6a6869381d81e8407e4c85420f
Rupali <78981177+rupali18082003@users.noreply.github.com>
2021-10-30 18:36:21 +0530

    updated

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 const ipAdd = document.querySelector('#ip');
 const go = document.querySelector('#go');
-const showIP = document.querySelector('.ip'); 
-const address = document.querySelector('.add'); 
-const timezone = document.querySelector('.timezone'); 
-const cc = document.querySelector('.cc'); 
+const showIP = document.querySelector('.ip');
+const address = document.querySelector('.add');
+const timezone = document.querySelector('.timezone');
+const cc = document.querySelector('.cc');
 
+let latitude, longitude;
 
-go.addEventListener('click', async function(){
-	const ip = ipAdd.value;
-	
-	fetch(`https://ipapi.co/${ip}/json/`)
-	.then(res => res.json())
-	.then(data => {
-		if(data.status == "fail"){
-			throw new Error("Invalid IP Address");
-		}
+go.addEventListener('click', async function() {
+    const ip = ipAdd.value;
 
-		showIP.textContent = data.ip;
-		address.textContent = data.city + ", " + data.region + ", " + data.country_name;
-		timezone.textContent = data.timezone;
-		cc.textContent = data.org;
-	})
-	.catch(err => alert(err.message))
+    fetch(`https://ipapi.co/${ip}/json/`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.status == "fail") {
+                throw new Error("Invalid IP Address");
+            }
+
+            //displaying content
+            showIP.textContent = data.ip;
+            address.textContent = data.city + ", " + data.region + ", " + data.country_name;
+            timezone.textContent = data.timezone;
+            cc.textContent = data.org;
+
+            //loading map
+            latitude = data.latitude;
+            longitude = data.longitude;
+
+            const coords = [latitude, longitude];
+            const map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            //displaying popup
+            L.marker(coords).addTo(map)
+                .bindPopup("IP: " + data.ip)
+                .openPopup();
+
+        })
+        .catch(err => alert(err.message))
 })
-
-/*
-//mine
-49.15.226.69
-//pinku
-102.64.48.0
-*/
